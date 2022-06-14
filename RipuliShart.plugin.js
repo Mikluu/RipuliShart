e6e08f0307405b775dcee7de6b094c8d3ce02f38
Mikluu <85846530+Mikluu@users.noreply.github.com>
2022-06-14 12:26:17 +0300

    lolzeing
    LOLOLOOOLOLOLOLOL

--- RipuliShart.plugin.js
+++ RipuliShart.plugin.js
@@ -1,10 +1,17 @@
 /**
  * @name RipuliShart
+ * @version 0.0.1
  * @author Miklu
  * @authorId 249860014460633098
- * @version 1.0.0
  * @description Idk testing some stuff
- * @invite Jx3TjNS
- * @source https://github.com/Mikluu
- * @updateUrl https://github.com/Mikluu
+ * @source https://github.com/Mikluu/RipuliShart
+ * @updateUrl https://github.com/Mikluu/RipuliShart/blob/main/RipuliShart.plugin.js
  */
+
+module.exports = class RipuliShart {
+    start() { 
+        console.log("RIPULIA JA UKKKIAS");
+        alert("LOL","aitis");
+    }
+    stop() { }
+}
